0bce1aeb6bb7a41bedbc88d32f951e765c52f8cb
James Tang <jamestang@Jamess-MacBook-Pro.local>
2020-10-17 15:23:52 -0400

    Error Handing and Expess Set Up

--- db/index.js
+++ db/index.js
@@ -2,8 +2,5 @@ const {Pool} = require('pg');
 const {user, host, database, password, port} = require('../secrets/db_configuration')
 const pool = new Pool({user, host,database,password, port});
 
-pool.query('SELECT * FROM monsters', (err, res) => {
-    if(err) return console.log(err);
 
-    console.log(res);
-});
+module.exports = pool;
